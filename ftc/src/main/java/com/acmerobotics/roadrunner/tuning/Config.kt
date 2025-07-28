20c51a2913a3f52fb4ebf44811d2bd9c50792943
0verkil <112268976+0verkil@users.noreply.github.com>
2025-07-28 14:00:38 -0700

    feat: config class

---
+++ ftc/src/main/java/com/acmerobotics/roadrunner/tuning/Config.kt
@@ -0,0 +1,91 @@
+package com.acmerobotics.roadrunner.tuning
+
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.config.ValueProvider
+import com.acmerobotics.dashboard.config.variable.BasicVariable
+import com.acmerobotics.dashboard.config.variable.ConfigVariable
+import com.acmerobotics.dashboard.config.variable.CustomVariable
+import com.google.gson.Gson
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil
+import java.io.File
+
+object HermesConfig : PersistentConfig("HermesConfig", AppUtil.ROOT_FOLDER.resolve("hermes/config/config.txt")) {
+
+}
+
+open class PersistentConfig(val configName: String, var configFile: File) {
+
+    private val configVariables: MutableMap<String, ValueProvider<Any?>> = mutableMapOf()
+
+    private val configWriter get() = PersistentConfigWriter(configFile)
+    private val configReader get() = PersistentConfigReader(configFile)
+
+    fun <T : Any?> addConfigVariable(name: String, value: T) {
+
+        val provider = PersistentConfigValueProvider<T>(value, this)
+        FtcDashboard.getInstance().addConfigVariable<T>(configName, name, provider)
+        configVariables.putIfAbsent(name, provider as ValueProvider<Any?>) // legal typecast since T is a subclass of Any?
+
+    }
+
+    operator fun get(key: String): Any? {
+        return configVariables[key]?.get()
+    }
+
+    fun getProvider(key: String): ValueProvider<Any?>? {
+        return configVariables[key]
+    }
+
+    operator fun <T: Any?> set(key: String, value: T): T? {
+        val provider = getProvider(key)
+        if (provider == null) {
+            addConfigVariable(key, value)
+            return value
+        }
+
+        try {
+            provider.set(value)
+        } catch (e: ClassCastException) {
+            return null
+        }
+
+        return value
+
+    }
+
+    fun updateConfig() {
+
+    }
+
+}
+
+class PersistentConfigValueProvider<T: Any?>(initalValue: T, val config: PersistentConfig) : ValueProvider<T> {
+    private var value: T? = initalValue
+
+    override fun get(): T? {
+        return value
+    }
+
+    override fun set(value: T?) {
+        this.value = value
+        config.updateConfig()
+    }
+
+}
+
+class PersistentConfigWriter(val configFile: File) {
+
+    private val gson = Gson()
+
+    fun writeConfig(config: PersistentConfig) {
+
+    }
+
+}
+
+class PersistentConfigReader(val configFile: File) {
+
+}
+
+
+val CustomVariable.values get() = this.value as Map<String, ConfigVariable<Any?>> // legal typecast because that's what the return actually is
