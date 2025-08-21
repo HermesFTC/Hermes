a22519c99e054fce1da4f91e411f6d5ae74eb872
0verkil <112268976+0verkil@users.noreply.github.com>
2025-08-20 21:25:03 -0700

    feat: pinpoint forward push view

--- ftc/src/main/java/com/acmerobotics/roadrunner/tuning/TuningOpModes.kt
+++ ftc/src/main/java/com/acmerobotics/roadrunner/tuning/TuningOpModes.kt
@@ -1,17 +1,30 @@
 package com.acmerobotics.roadrunner.tuning
 
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.config.ValueProvider
+import com.acmerobotics.dashboard.config.reflection.FieldProvider
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 
 class ForwardPushTest() : OpMode() {
 
     override fun init() {
-
+        FtcDashboard.getInstance().addConfigVariable(
+            "ForwardPush", "Actual Inches",
+            FieldProvider<Double>(
+                Companion.javaClass.getField("actualInchesTravelled"), // CURSED SHIT
+                Companion,
+            ),
+        )
     }
 
     override fun loop() {}
 
     override fun stop() {
 
+    }
+
+    companion object {
+        var actualInchesTravelled = 24.0
     }
 
 }
