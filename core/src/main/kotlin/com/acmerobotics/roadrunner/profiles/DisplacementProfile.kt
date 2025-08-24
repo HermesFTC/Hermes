670d5d0914bc7cfd618183151b6a451fcc01c9a3
0verkil <112268976+0verkil@users.noreply.github.com>
2025-08-24 13:15:16 -0700

    feat: voltage-based motion profiling

--- core/src/main/kotlin/com/acmerobotics/roadrunner/profiles/DisplacementProfile.kt
+++ core/src/main/kotlin/com/acmerobotics/roadrunner/profiles/DisplacementProfile.kt
@@ -375,6 +375,162 @@ fun backwardProfile(
     )
 }
 
+/**
+ * Computes an approximately time-optimal profile with the given constraints and resolution.
+ *
+ * @param[beginEndVel] beginning and ending velocity, non-negative (must be the same to guarantee feasibility)
+ * @param kV Unit of velocity per unit of voltage (u voltage should stabilize at u * kV velocity)
+ * @param kA Unit of acceleration per unit of voltage (starting from 0, u voltage should accelerate at u * kA acceleration)
+ * @param kS Static voltage offset, to account for forces like static friction. Nonnegative.
+ * @param maxVoltage Always returns positive
+ */
+fun profile(
+    length: Double,
+    beginEndVel: Double,
+    kV: Double,
+    kA: Double,
+    kS: Double,
+    maxVoltage: (Double) -> Double,
+    resolution: Double,
+): CancelableProfile {
+    require(length > 0.0) { "length ($length) must be positive" }
+    require(resolution > 0.0) { "resolution ($resolution) must be positive" }
+    require(beginEndVel >= 0.0) { "beginEndVel ($beginEndVel) must be non-negative" }
+
+    val samples = max(1, ceil(length / resolution).toInt())
+
+    val disps = rangeCentered(0.0, length, samples)
+
+    // subtract off kS here to be lazy
+    val actualMaxVoltages = disps.map { maxVoltage(it) - kS }
+
+    val cancellationMinAccels = actualMaxVoltages.map { -kA * it }
+
+    return CancelableProfile(
+        merge(
+            forwardProfile(disps, beginEndVel, kV, kA, actualMaxVoltages),
+            backwardProfile(disps, beginEndVel, kV, kA, actualMaxVoltages),
+        ),
+        disps, cancellationMinAccels
+    )
+}
+
+/**
+ * Computes an approximately time-optimal forward profile from voltage constraints. No restriction is imposed
+ * on the minimum acceleration.
+ *
+ * The procedure uses me and not ryan brott's procedure because having disp-based max vel and acc is stupid.
+ *
+ * @param[disps] displacement interval endpoints
+ * @param[beginVel] beginning velocity, non-negative
+ * @param[maxVoltages] all positive
+ */
+@Suppress("NAME_SHADOWING")
+fun forwardProfile(
+    disps: List<Double>,
+    beginVel: Double,
+    kV: Double,
+    kA: Double,
+    maxVoltages: List<Double>
+): DisplacementProfile {
+    require(beginVel >= 0.0) { "beginVel ($beginVel) must be non-negative" }
+    require(maxVoltages.all { v -> v > 0.0 }) { "maxVoltages must be positive" }
+
+    val newDisps = mutableListOf(0.0)
+    val vels = mutableListOf(beginVel)
+    val accels = mutableListOf<Double>()
+
+    val getVel = velocity(kV)
+    val getAccel = acceleration(kA)
+
+    val getVelVolt = voltageFromVelocity(kV)
+
+    maxVoltages
+        .zip(disps.drop(1))
+        .fold(disps[0]) { beginDisp, (maxVoltage, endDisp) ->
+            val beginVel = vels.last()
+            val maxVel = getVel(maxVoltage)
+            val maxAccel = getAccel(maxVoltage - getVelVolt(beginVel))
+
+            // if our vel is greater than the constraint, drop to the constraint (backwards pass will take care of accel here)
+            if (beginVel >= maxVel) {
+                newDisps.add(endDisp)
+                vels.add(maxVel)
+                accels.add(0.0)
+            } else {
+                // approximate using constant accel (from initial velocity)
+                val endVel = sqrt(beginVel * beginVel + 2.0 * maxAccel * (endDisp - beginDisp))
+
+                // if less than max vel, add state
+                if (endVel <= maxVel) {
+                    newDisps.add(endDisp)
+                    vels.add(endVel)
+                    accels.add(maxAccel)
+                } else {
+                    // otherwise, go only as far as necessary to reach max velocity and add a new sample there to ensure optimality
+                    val accelDx = (maxVel * maxVel - beginVel * beginVel) / (2 * maxAccel)
+
+                    newDisps.add(beginDisp + accelDx)
+                    vels.add(maxVel)
+                    accels.add(maxAccel)
+
+                    // then append the original sample with the same velocity
+                    newDisps.add(endDisp)
+                    vels.add(maxVel)
+                    accels.add(0.0)
+                }
+            }
+
+            // set the accumulator to endDisp to ensure the next beginDisp is correct
+            endDisp
+        }
+
+    return DisplacementProfile(newDisps, vels, accels)
+}
+
+/**
+ * Computes an approximately time-optimal backward profile from voltage constraints. No restriction is imposed
+ * on the maximum acceleration.
+ *
+ * The procedure uses me and not ryan brott's procedure because having disp-based max vel and acc is stupid.
+ *
+ * @param[disps] displacement interval endpoints
+ * @param[endVel] ending velocity, non-negative
+ * @param[maxVoltages] all positive
+ */
+fun backwardProfile(
+    disps: List<Double>,
+    endVel: Double,
+    kV: Double,
+    kA: Double,
+    maxVoltages: List<Double>,
+) = forwardProfile(
+    disps.reversed().map { disps.last() - it }, endVel,
+    kV, kA, maxVoltages.reversed()
+).let {
+    DisplacementProfile(
+        it.disps.map { x -> it.length - x }.reversed(),
+        it.vels.reversed(),
+        it.accels.reversed().map { a -> -a },
+    )
+}
+
+fun velocity(kV: Double): (Double) -> Double {
+    return {availableVoltage: Double -> availableVoltage * kV}
+}
+
+fun voltageFromVelocity(kV: Double): (Double) -> Double {
+    return {velocity: Double -> velocity / kV}
+}
+
+fun acceleration(kA: Double): (Double) -> Double {
+    return {availableVoltage: Double -> availableVoltage * kA}
+}
+
+fun voltageFromAcceleration(kA: Double): (Double) -> Double {
+    return {velocity: Double -> velocity / kA}
+}
+
 operator fun DisplacementProfile.plus(other: DisplacementProfile): DisplacementProfile {
     require(this.vels.last() == other.vels.first()) { "this.vels.last() (${this.vels.last()}) != other.vels.first() (${other.vels.first()})" }
 
