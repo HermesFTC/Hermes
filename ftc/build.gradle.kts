4ba00f989e5c2d72776f82ff670c71690846a129
Zach Harel <zach@zharel.me>
2025-08-31 17:45:22 -0400

    Update build configuration and version to 0.3.0-local; streamline deployer settings and add subproject configuration
    Signed-off-by: Zach Harel <zach@zharel.me>

--- ftc/build.gradle.kts
+++ ftc/build.gradle.kts
@@ -14,7 +14,6 @@ plugins {
     alias(libs.plugins.dokka)
 
     `maven-publish`
-    alias(libs.plugins.deployer)
 
     id("com.github.node-gradle.node") version "2.2.4"
 }
@@ -130,24 +129,8 @@ val dokkaJar = tasks.register<Jar>("dokkaJar") {
 
 deployer {
     projectInfo {
-        groupId.set("me.zharel.hermes")
         artifactId.set("ftc")
-
-        name.set("Hermes")
         description.set("Integration of Hermes' core library with FTC.")
-        url.set("https://github.com/HermesFTC/Hermes")
-        scm {
-            fromGithub("HermesFTC", "Hermes")
-        }
-        license("MIT License", "https://opensource.org/license/mit")
-
-        developer("Zachary Harel", "ftc@zharel.me", url = "https://github.com/zachwaffle4")
-        developer("Ryan Brott", "rcbrott@gmail.com", url = "https://github.com/rbrott")
-    }
-
-    signing {
-        key.set(secret("MVN_GPG_KEY"))
-        password.set(secret("MVN_GPG_PASSWORD"))
     }
 
     content {
@@ -155,26 +138,5 @@ deployer {
             kotlinSources()
             docs(dokkaJar)
         }
-    }
-
-    localSpec {
-        release.version.set("$version")
-    }
-
-    nexusSpec("snapshot") {
-        release.version.set("$version")
-        repositoryUrl.set("https://central.sonatype.com/repository/maven-snapshots/")
-        auth {
-            user.set(secret("SONATYPE_USERNAME"))
-            password.set(secret("SONATYPE_PASSWORD"))
-        }
-    }
-
-    centralPortalSpec {
-        auth {
-            user.set(secret("SONATYPE_USERNAME"))
-            password.set(secret("SONATYPE_PASSWORD"))
-        }
-        allowMavenCentralSync.set((property("automaticMavenCentralSync") as String).toBoolean())
     }
 }
